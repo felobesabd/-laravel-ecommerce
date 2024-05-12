e396936a55b96e3ea360225df472b2f040df597a
felobesabd <felobesaddd@gmail.com>
2024-05-12 23:31:32 +0300

    start product migration&seeder&factory&model&controller&request&routes

---
+++ app/Models/Product.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use Translatable;
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = ['translations'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'brand_id',
+        'slug',
+        'sku',
+        'price',
+        'special_price',
+        'special_price_type',
+        'special_price_start',
+        'special_price_end',
+        'selling_price',
+        'manage_stock',
+        'qty',
+        'in_stock',
+        'is_active'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'manage_stock' => 'boolean',
+        'in_stock' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+
+    /**
+     * The attributes that are translatable.
+     *
+     * @var array
+     */
+    protected $translatedAttributes = ['name', 'description', 'short_description'];
+
+    public function getActive()
+    {
+        return $this->is_active == 0 ? __('dashboard.not_active') : __('dashboard.active');
+    }
+}
---
+++ app/Repositories/Repository.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Interfaces\RepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class Repository implements RepositoryInterface
+{
+    protected $model;
+
+    /**
+     * Repository constructor.
+     * @param $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $record = $this->find($id);
+        return $record->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    public function show($id)
+    {
+        return $this->model-findOrFail($id);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param mixed $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    public function with($relations)
+    {
+        return $this->model->with($relations);
+    }
+
+}
